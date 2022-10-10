d6474e93a58fad316a4226ff5d55319684529a52
Mateusz Kaczorowski <mmssfd0@gmail.com>
2022-10-10 20:00:00 +0200

    add else if blocks

--- js/script.js
+++ js/script.js
@@ -23,48 +23,35 @@ let playerMove = "nieznany ruch";
 
 if (playerInput == "1") {
   playerMove = "kamień";
-}
-if (playerInput == "2") {
+} else if (playerInput == "2") {
   playerMove = "nożyce";
-}
-if (playerInput == "3") {
+} else if (playerInput == "3") {
   playerMove = "papier";
 }
 
 printMessage("Twój ruch to: " + playerMove);
 if (computerMove == "kamień" && playerMove == "papier") {
   printMessage("Ty wygrywasz!");
-}
-if (computerMove == "nożyce" && playerMove == "papier") {
+} else if (computerMove == "nożyce" && playerMove == "papier") {
   printMessage("Ty przegrywasz!");
-}
-if (computerMove == "papier" && playerMove == "papier") {
+} else if (computerMove == "papier" && playerMove == "papier") {
   printMessage("Remis!");
-}
-if (computerMove == "kamień" && playerMove == "nożyce") {
+} else if (computerMove == "kamień" && playerMove == "nożyce") {
   printMessage("Ty przegrywasz!");
-}
-if (computerMove == "nożyce" && playerMove == "nożyce") {
+} else if (computerMove == "nożyce" && playerMove == "nożyce") {
   printMessage("Remis!");
-}
-if (computerMove == "papier" && playerMove == "nożyce") {
+} else if (computerMove == "papier" && playerMove == "nożyce") {
   printMessage("Ty wygrywasz");
-}
-if (computerMove == "kamień" && playerMove == "kamień") {
+} else if (computerMove == "kamień" && playerMove == "kamień") {
   printMessage("Remis!");
-}
-if (computerMove == "nożyce" && playerMove == "kamień") {
+} else if (computerMove == "nożyce" && playerMove == "kamień") {
   printMessage("Ty wygrywasz!");
-}
-if (computerMove == "papier" && playerMove == "kamień") {
+} else if (computerMove == "papier" && playerMove == "kamień") {
   printMessage("Ty przegrywasz!");
-}
-if (computerMove == "papier" && playerMove == "nieznany ruch") {
+} else if (computerMove == "papier" && playerMove == "nieznany ruch") {
   printMessage("Ty przegrywasz!");
-}
-if (computerMove == "kamień" && playerMove == "nieznany ruch") {
+} else if (computerMove == "kamień" && playerMove == "nieznany ruch") {
   printMessage("Ty przegrywasz!");
-}
-if (computerMove == "nożyce" && playerMove == "nieznany ruch") {
+} else if (computerMove == "nożyce" && playerMove == "nieznany ruch") {
   printMessage("Ty przegrywasz!");
 }
