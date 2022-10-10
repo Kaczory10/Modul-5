e7537c2fd1206d1f181e65fde26e68cb9dd5842e
Mateusz Kaczorowski <mmssfd0@gmail.com>
2022-10-10 04:04:06 +0200

    computermove

--- js/script.js
+++ js/script.js
@@ -7,6 +7,20 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+if(randomNumber == 2){
+    computerMove = 'nożyce';
+  }
+  if(randomNumber == 3){
+    computerMove = 'papier';
+  }
+
+printMessage('Mój ruch to: ' + computerMove);
